3999f23937a299179d8d0a52b730590a799ba828
unknown <victorestares80@gmail.com>
2022-10-21 02:41:35 -0300

    actualizacion

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -o errexit
 
 pip install -r requerimientos.txt
 
-python manage.py createsuperuser
-victor
-victorestares80@gmail.com
-Flavia123
-Flavia123
+
 python manage.py collectstatic --no-input
 python manage.py migrate
