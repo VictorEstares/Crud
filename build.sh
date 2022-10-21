aac8078114211e07969e6addcda2f8d45bd39f75
unknown <victorestares80@gmail.com>
2022-10-21 02:39:54 -0300

    listo para subir

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ set -o errexit
 pip install -r requerimientos.txt
 
 python manage.py createsuperuser
-username: victor
-email: victorestares80@gmail.com
-password: Flavia123
-password: Flavia123
+victor
+victorestares80@gmail.com
+Flavia123
+Flavia123
 python manage.py collectstatic --no-input
 python manage.py migrate
